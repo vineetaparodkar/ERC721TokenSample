f929794c27a31631c61c3e2df2e303423e183a32
vineetaparodkar <>
2022-04-30 21:27:45 +0530

    ipfs module refactor

--- scripts/ipfs-script.js
+++ scripts/ipfs-script.js
@@ -1,21 +1,30 @@
 const { NFTStorage, File } = require("nft.storage");
 const fs = require("fs");
+const mime = require("mime")
+const path = require("path")
+
 
 require("dotenv").config();
 const { API_KEY } = process.env;
-
 const client = new NFTStorage({ token: API_KEY });
 
+const fileFromPath = async (filePath) => {
+  const content = await fs.promises.readFile(filePath);
+  const type = mime.getType(filePath);
+  return new File([content], path.basename(filePath), { type });
+};
+
 const generateMetadata = async () => {
+  const image = await fileFromPath("./assets/pexels-daniel-dan-7708818.jpg");
+  const name = "Blockchain NFT";
+  const description = "PEXELS DANIEL DAN IMAGE!";
+
   const metadata = await client.store({
-    name: "Blockchain NFT",
-    description: "PEXELS DANIEL DAN IMAGE!",
-    image: new File(
-      [await fs.promises.readFile("./assets/pexels-daniel-dan-7708818.jpg")],
-      "pexels-daniel-dan-7708818.jpg",
-      { type: "image/jpg" }
-    ),
+    name,
+    description,
+    image,
   });
+
   console.log("Metadata ipnft: " + metadata.ipnft);
   return metadata.ipnft;
 };
